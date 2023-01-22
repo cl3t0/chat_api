5484462782a121c9e84903eca6997039d3480516
Pedro Cleto <cl3t0.sh@gmail.com>
2023-01-22 19:43:45 -0300

    feat: base project from phoenix

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :chat_api, ChatApi.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "chat_api_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :chat_api, ChatApiWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "MgagyRjh9cMQM9F09wKnyQyDKF1Fw46Zk6laVA+PCdSCvBC5GmCVMvtMuMmXcfIh",
+  server: false
+
+# In test we don't send emails.
+config :chat_api, ChatApi.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
